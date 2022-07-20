19c1134860ade9e6f6736265a76a940a9b55cb4e
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-07-20 23:52:50 +0300

    работа с объектом дата

--- training/setTimeout/script.js
+++ training/setTimeout/script.js
@@ -1,27 +1,3 @@
-const btn = document.querySelector('.btn');
-let timerId;
-let i = 0;
-
-function myAnimation () {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-
-    const id = setInterval(frame, 10);
-
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-           pos++;
-           elem.style.top = pos + 'px'; 
-           elem.style.left = pos + "px"; 
-        }
-    }
-}
-
-btn.addEventListener('click', myAnimation);
-    
-
 // btn.addEventListener('click', () => {
 //     // const timeId = setTimeout(logger, 2000);
 //     timerId = setInterval(logger, 500);
@@ -40,3 +16,49 @@ btn.addEventListener('click', myAnimation);
 //     console.log('hello');
 //     id = setTimeout(log, 500);
 // }, 500);
+
+                           // Анимация на setInterval
+
+// const btn = document.querySelector('.btn');
+
+// function myAnimation () {
+//     const box = document.querySelector('.box');
+//     let pos = 0;
+
+//     const id = setInterval(frame, 4);
+//     function frame () {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             box.style.left = pos + 'px';
+//             box.style.top = pos + 'px';
+//         }
+//     }
+// }
+ 
+// btn.addEventListener('click', myAnimation);
+
+                            // работа с объектом дата 
+
+const now = new Date();
+// console.log(now);
+// console.log(now.getFullYear());  
+// console.log(now.getMonth());   
+// console.log(now.getDate());            
+
+// console.log(now.getTime());
+
+// console.log(now.setHours(23));
+// console.log(now);
+
+let start = new Date();
+
+for (let i = 0; i < 1000000; i++) {
+    let some = i ** 3;
+}
+let end = new Date();
+
+const result = end - start;
+
+console.log(`операция заняла ${result} милисекунд`);
