3549b5cab5093bcebc37cb1623066df64225c4a2
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-07-25 21:24:11 +0300

    исправления таймера при отрицательных значениях

--- training/Food/js/script.js
+++ training/Food/js/script.js
@@ -44,12 +44,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2022-07-30';
     
     function getTimeRemeining(endtime) {
+        let days, hours, minutes, seconds;
         const t = Date.parse(endtime) - Date.parse(new Date());
-        const days = Math.floor(t/(1000 * 60 * 60 * 24));
-        const hours = Math.floor((t / (1000 * 60 * 60) % 24));
-        const minutes = Math.floor((t / 1000 / 60) % 60);
-        const seconds = Math.floor((t / 1000) % 60 );
 
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t/(1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60) % 24));
+            minutes = Math.floor((t / 1000 / 60) % 60);
+            seconds = Math.floor((t / 1000) % 60 );
+        }
+      
         return {
             'total': t,
             'days': days,
