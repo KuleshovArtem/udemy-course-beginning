4c8e210c7eaefd9e00518ba60640af6c8db55f45
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-09-03 08:20:07 +0300

    работа с AJAX через XMLHttpRequest

---
+++ training/calc/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    request.addEventListener('load', ()=>{
+        if ( request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Что-то пошло не так';
+        }
+    });
+    
+});
