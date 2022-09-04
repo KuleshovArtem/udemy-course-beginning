8a6de2f27aa47b8a9e8b9c75db2fe1b02e537e93
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-09-04 08:56:31 +0300

    реализация отправки данных на сервер

--- training/Food/js/script.js
+++ training/Food/js/script.js
@@ -216,17 +216,60 @@ window.addEventListener('DOMContentLoaded', () => {
         
     ).render();
 
+    //Forms 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            // request.send(formData);
+
+            request.addEventListener('load', () => {
+                if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(()=> {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
 
-const log = function(a, b, ...rest) {
-    console.log(a, b, rest);
-}
-
-log('basic', 'rest', 'operator', 'usage');
-
-function calcOrDouble(number, basis = 2) {
-    // basis= basis || 2;
-
-    console.log(number * basis);
-}
-calcOrDouble(3);
